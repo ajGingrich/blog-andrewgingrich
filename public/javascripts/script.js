f84c5427e25f53431cb9405cb9ca1c1a071fa218
Andrew Gingrich <gandrew320@gmail.com>
2017-10-23 09:30:03 -0600

    remove mongodb and extraneous information

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,3 @@
-$('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true
-});
-
 function defaultImage(image) {
     image.onerror = "";
     image.src = "/img/broken.jpg";
