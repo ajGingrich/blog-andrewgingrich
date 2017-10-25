0194937d0457469b085c5d8bc9fe82b25ec8e9c2
Andrew Gingrich <gandrew320@gmail.com>
2017-10-25 13:39:47 -0600

    update for production

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ import path from 'path';
 import fs from 'fs';
 // var favicon = require('serve-favicon');
 import logger from 'morgan';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
 import webpack from 'webpack';
 
 const config = require('../webpack.config');
@@ -12,24 +10,24 @@ const compiler = webpack(config);
 const routes = require('./routes/index');
 
 const app = express();
+const isDevelopment  = app.get('env') !== "production";
 
+if (isDevelopment) {
+    //logger with morgan
+    //set up webpack middleware
+    app.use(require('webpack-dev-middleware')(compiler, {
+        noInfo: true, publicPath: config.output.publicPath
+    }));
+    app.use(require('webpack-hot-middleware')(compiler));
+
+}
 // view engine setup
 app.set('view engine', 'html');
 app.engine('html', function (path, options, callbacks) {
     fs.readFile(path, 'utf-8', callback)
 });
 
-//set up webpack middleware
-app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true, publicPath: config.output.publicPath
-}));
-app.use(require('webpack-hot-middleware')(compiler));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
-
 app.use('/', routes);
 
 // catch 404 and forward to error handler
