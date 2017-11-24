610749280651c3647a4d4b01f5019282320949ea
Andrew Gingrich <ajgingrich@ole.org>
2017-11-23 18:42:36 -0600

    update tab indents

--- client/containers/PostContainer.js
+++ client/containers/PostContainer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import Link from './Link';
 import { Link } from 'react-router-dom';
 import Butter from 'buttercms';
 
@@ -8,69 +7,68 @@ const butter = Butter(process.env.BUTTERCMS_KEY);
 
 class PostContainer extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loaded: false
-        }
+    this.state = {
+      loaded: false
     }
+  }
 
-    fetchPosts(page) {
-        butter.post.list({page: page, page_size: 10}).then((resp) => {
-            this.setState({
-                loaded: true,
-                resp: resp.data
-            })
-        });
-    }
+  fetchPosts(page) {
+    butter.post.list({page: page, page_size: 10}).then((resp) => {
+      this.setState({
+        loaded: true,
+        resp: resp.data
+      })
+    });
+  }
 
-    componentWillMount() {
-        let page = this.props.page;
-        this.fetchPosts(page)
-    }
+  componentWillMount() {
+    let page = this.props.page;
+    this.fetchPosts(page)
+  }
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({loaded: false});
+  componentWillReceiveProps(nextProps) {
+    this.setState({loaded: false});
 
-        let page = nextProps.page;
-        this.fetchPosts(page)
-    }
+    let page = nextProps.page;
+    this.fetchPosts(page)
+  }
 
-    render() {
-        if (this.state.loaded) {
-            const { next_page, previous_page } = this.state.resp.meta;
+  render() {
+    if (this.state.loaded) {
+      const { next_page, previous_page } = this.state.resp.meta;
 
-            return (
-                <div className="col-xs-12 col-md-9">
-                    <div className="post">
-                        {this.state.resp.data.map((post) => {
-                            return (
-                                <div key={post.slug}>
-                                    <Link to={`/article/${post.slug}`}><h1 className="postTitle">{post.title}</h1></Link>
-                                    <div>{post.summary}</div>
-                                </div>
-                            )
-                        })}
-                        <br />
-
-                        {/*<div>
-                            {previous_page && <Link to={`/p/${previous_page}`}>Prev</Link>}
-                            {next_page && <Link to={`/p/${next_page}`}>Next</Link>}
-                        </div>*/}
-                    </div>
+      return (
+        <div className="col-xs-12 col-md-9">
+          <div className="post">
+            {this.state.resp.data.map((post) => {
+              return (
+                <div key={post.slug}>
+                  <Link to={`/article/${post.slug}`}><h1 className="postTitle">{post.title}</h1></Link>
+                    <div>{post.summary}</div>
                 </div>
-            )
-        } else {
-            return (
-                <div className="col-xs-12 col-md-9">
-                    <div className="post">
-                        Loading...
-                    </div>
-                </div>
-            )
-        }
+              )
+            })}
+            <br />
+            {/*<div>
+              {previous_page && <Link to={`/p/${previous_page}`}>Prev</Link>}
+              {next_page && <Link to={`/p/${next_page}`}>Next</Link>}
+              </div>*/}
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div className="col-xs-12 col-md-9">
+          <div className="post">
+            Loading...
+          </div>
+        </div>
+      )
     }
+  }
 }
 
 export default PostContainer
