1a231489888e032166a63e3808c2e99c62ebf622
Andrew Gingrich <gandrew320@gmail.com>
2017-11-30 20:08:11 -0600

    test https

--- server/server.js
+++ server/server.js
@@ -50,6 +50,17 @@ app.use(function(req, res, next) {
 });
 
 const port = process.env.PORT || 52000;
+
+app.enable('trust proxy');
+app.use (function (req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
 app.listen(port,  function () {
     console.log('Node.js listening on port ' + port + '...');
 });
