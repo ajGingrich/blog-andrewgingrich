3d896198921f38a8f4e354b31dc2015bbcbb9d28
Andrew Gingrich <gandrew320@gmail.com>
2017-12-23 20:40:55 -0600

    test disqus live

--- client/containers/FullPost.js
+++ client/containers/FullPost.js
@@ -2,10 +2,11 @@ import React from 'react';
 //import ReactDOM from 'react-dom';
 import { Link } from 'react-router';
 import Butter from 'buttercms';
+//import Disqus from '../components/Disqus'
+import ReactDisqusComments from 'react-disqus-comments';
 //import Highlight from 'react-highlight'
 
 const butter = Butter(process.env.BUTTERCMS_KEY);
-
 
 class FullPost extends React.Component {
 
@@ -26,6 +27,10 @@ class FullPost extends React.Component {
     });
   }
 
+  handleNewComment(comment) {
+    console.log(comment.text);
+  }
+
   componentWillMount() {
     let slug = this.props.slug;
     this.fetchPost(slug);
@@ -40,6 +45,8 @@ class FullPost extends React.Component {
     render() {
       if (this.state.loaded) {
         const post = this.state.post;
+        const shortname = "andrewgingrich";
+        const categoryId = 'test';
 
         return (
           <div className="col-xs-12 col-md-9">
@@ -47,6 +54,14 @@ class FullPost extends React.Component {
               <div className="postContainer">
                 <h1>{post.title}</h1>
                 <div dangerouslySetInnerHTML={{__html: post.body}} />
+                <ReactDisqusComments
+                  shortname={shortname}
+                  identifier={post.published}
+                  title={post.title}
+                  url={post.url}
+                  category_id={categoryId}
+                  onNewComment={this.handleNewComment}
+                />
               </div>
             </div>
           </div>
