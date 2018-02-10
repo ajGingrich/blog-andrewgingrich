f73e43be725b0f0c79aca0306775363649d549ab
Andrew Gingrich <ajgingrich@ole.org>
2018-02-10 12:05:15 -0600

    add hamburger mobile search styles

--- client/components/Search/NavbarSearch.js
+++ client/components/Search/NavbarSearch.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { NavLink, Redirect } from 'react-router-dom'
 import { withRouter } from 'react-router'
+import { connect } from 'react-redux'
+import { action as toggleMenu } from 'redux-burger-menu'
+import store from 'store'
 
+function browserSelector({browser}) {
+  return {browser}
+}
+
+@connect(browserSelector)
 class NavbarSearch extends React.Component {
 
   constructor(props) {
@@ -20,6 +28,11 @@ class NavbarSearch extends React.Component {
       value: event.target.value,
       submitted: false
     });
+  }
+
+  _handleCloseMenu() {
+    const isOpen = false
+    store.dispatch(toggleMenu(isOpen));
   }
 
   _handleSubmit(event) {
@@ -44,17 +57,25 @@ class NavbarSearch extends React.Component {
 
   render() {
     const { submitted, value } = this.state
-    const { history } = this.props
+    const { browser, history } = this.props
+    const isBrowserLessThanLarge = browser.lessThan.large
 
     return (
       <div className='searchForm'>
         <form onSubmit={this._handleSubmit}>
-          <label>
+          <label className="navbarSearchLabel">
             <input type="text" value={value} onChange={this._handleChange} placeholder="Search..." />
           </label>
-          <span onClick={this._handleSubmit} className ="tooltip-bottom" data-tooltip="Search">
-            <i className="fa fa-search fa-lg" />
-          </span>
+          {isBrowserLessThanLarge &&
+            <span className='mobileSearchButton' onClick={this._handleSubmit && this._handleCloseMenu}>
+              <button className='btn btn-primary'>Go</button>
+            </span>
+          }
+          {!isBrowserLessThanLarge &&
+            <span onClick={this._handleSubmit} className ="tooltip-bottom" data-tooltip="Search">
+              <i className="fa fa-search fa-lg" />
+            </span>
+          }
         </form>
       </div>
     );
