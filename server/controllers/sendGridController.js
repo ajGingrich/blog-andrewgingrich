f6a3af4e24fcd55539f15d0728be98835fe3b1d7
Andrew Gingrich <ajgingrich@yahoo.com>
2018-02-12 18:27:50 -0600

    work on firebase [wip]

--- server/controllers/sendGridController.js
+++ server/controllers/sendGridController.js
@@ -1,8 +1,22 @@
 import axios from 'axios';
 import CircularJSON from 'circular-json';
-import {
+/*import {
   getNextCampaignId
-} from '../helpers/sendgrid'
+} from '../helpers/sendgrid'*/
+import * as firebase from "firebase";
+const firebaseDatabaseUrl = "https://blog-9ccca.firebaseio.com";
+
+var firebaseConfig = {
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: firebaseDatabaseUrl,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID
+};
+firebase.initializeApp(firebaseConfig);
+
+// const database = firebase.database();
 
 
 exports.addContact = function(req, res, next) {
@@ -23,6 +37,7 @@ exports.addContact = function(req, res, next) {
     }).catch(function (error) {
       const errorClone = CircularJSON.stringify(error)
       console.log(errorClone, 'error');
+
       return next(errorClone);
     });
 }
@@ -30,7 +45,11 @@ exports.addContact = function(req, res, next) {
 exports.sendEmail = function(req, res, next) {
   const isPostCreated = req.body
   const data = JSON.stringify({})
-  const sendGridCampaignId = getNextCampaignId()
+  // const sendGridCampaignId = getNextCampaignId()
+  console.log('before')
+  getSendGridCampaigns()
+  console.log('after')
+  const sendGridCampaignId = '2384144'
   const sendUrl = 'https://api.sendgrid.com/v3/campaigns/' + sendGridCampaignId + '/schedules/now'
   const config = {
     headers: {
@@ -44,13 +63,34 @@ exports.sendEmail = function(req, res, next) {
 
         const responseClone = CircularJSON.stringify(response)
         console.log(responseClone, 'response')
+
         Promise.resolve(response)
       }).catch(function (error) {
         const errorClone = CircularJSON.stringify(error)
         console.log(errorClone, 'error');
+
         return next(errorClone);
       });
   }
 
-  //acknowledge webhook receipt here with 200, or if not response with 300 or whatever
+  function getSendGridCampaigns() {
+    // console.log('entering')
+    // firebase.database().ref('/rest/blog/sendgridCampaign/').once('ids')
+    //   .then(function(data) {
+    //     console.log(data, 'data')
+    //   }).catch(function(error) {
+    //     console.log(error)
+    //   });
+    axios.get('https://blog-9ccca.firebaseio.com/blog/sendgridCampaign.json?auth=hiding')
+      .then(function(response) {
+        const campaignIds = response.data.ids
+        console.log(campaignIds[0].toString(), 'ids')
+        //Promise resolve/return?
+      }).catch(function(error) {
+        console.log(error, 'error')
+        //Promise reject error here?
+      });
+  }
+
+  //shift one and then post rest of it
 }
