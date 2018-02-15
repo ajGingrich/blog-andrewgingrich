f26f898c4703751afad1bac89b47ffad2abc7245
Andrew Gingrich <ajgingrich@yahoo.com>
2018-02-14 18:06:11 -0600

    update promise and scopes

--- server/controllers/firebaseController.js
+++ server/controllers/firebaseController.js
@@ -5,7 +5,10 @@ const Promise = require("bluebird");
 import axios from 'axios'
 
 const serviceAccount = require('./serviceAccountKey.json')
-const scopes = ["https://www.googleapis.com/auth/firebase.database"]
+const scopes = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/firebase.database"
+];
 const databaseUrl = "https://blog-9ccca.firebaseio.com/blog/sendgridCampaign.json"
 
 // Authenticate a JWT client with the service account.
@@ -18,31 +21,35 @@ const jwtClient = new google.google.auth.JWT(
 
 const getFirebaseToken = function() {
   // Use the JWT client to generate an access token.
-  jwtClient.authorize(function(error, tokens) {
-    if (error) {
-      console.log("Error making request to generate access token:", error);
-    } else if (tokens.access_token === null) {
-      console.log("Provided service account does not have permission to generate access tokens");
-    } else {
-      console.log(tokens.access_token, 'token baby')
-      return tokens.access_token;
-    }
-  });
+  return new Promise(function(resolve, reject) {
+    jwtClient.authorize(function(error, tokens) {
+      if (error) {
+        reject("Provided service account does not have permission to generate access tokens")
+      } else if (tokens.access_token === null) {
+        reject("Provided service account does not have permission to generate access tokens")
+      } else {
+        resolve(tokens.access_token)
+      }
+    });
+  })
 }
 
 exports.getSendGridCampaigns = function() {
+    const access_token = getFirebaseToken()
 
-    const access_token = getFirebaseToken();
-    //need a promise here
-    
-    console.log(access_token, 'another token')
-    axios.get(databaseURL + '?access_token=' + access_token)
-      .then(function(response) {
-        const campaignIds = response.data.ids
-        console.log(campaignIds[0].toString(), 'ids')
-        //Promise resolve/return?
-      }).catch(function(error) {
-        console.log(error, 'error')
-        //Promise reject error here?
-      });
+    access_token.then(function(token) {
+      axios.get(databaseUrl + '?access_token=' + token)
+        .then(function(response) {
+          const campaignIds = response.data.ids
+          console.log(campaignIds[0].toString(), 'ids')
+          //Promise resolve/return?
+        }).catch(function(error) {
+          console.log(error, 'error')
+          //Promise reject error here?
+        });
+
+        //return a promise here? returnnnnn
+    }).catch(function(error) {
+      console.log(error ,'this is an error')
+    })
   }
