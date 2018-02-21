99e74faaa7508d0011d936a0d821708ada862b94
Andrew Gingrich <ajgingrich@yahoo.com>
2018-02-20 18:27:10 -0600

    use connect in routes file to expand post width

--- client/containers/Sidebar.js
+++ client/containers/Sidebar.js
@@ -4,11 +4,11 @@ import { connect } from 'react-redux';
 import { AccordionPosts, SubscriptionWrapper } from 'Components'
 import { defaultBreakpoints } from 'constants/constants'
 
-function browserSelector({browser}) {
-  return {browser}
+function stateSelector({ browser, sidebar }) {
+  return { browser, sidebar}
 }
 
-@connect(browserSelector)
+@connect(stateSelector)
 class Sidebar extends React.Component {
 
   constructor(props) {
@@ -16,11 +16,12 @@ class Sidebar extends React.Component {
   }
 
   render() {
-    const { browser, isMobile } = this.props;
+    const { browser, isMobile, sidebar } = this.props;
     const sidebarWidth = browser.is.infinity ? '262.5px ': '212.5px'
     const sidebarStyles = browser.lessThan.large ? {} : { width: sidebarWidth }
+    const renderSidebar = sidebar.isOpen && (!browser.lessThan.large || isMobile)
 
-    if (!browser.lessThan.large || isMobile) {
+    if (renderSidebar) {
       return (
         <div className="col-md-3">
           <div className="sidebar" style={sidebarStyles}>
--- client/routes.js
+++ client/routes.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux';
 import {
   HashRouter as Router,
   Route,
@@ -17,33 +18,46 @@ import {
 } from 'Containers';
 import { FourOhFourError } from 'Components'
 
-const Routes = () => (
-  <Router>
-    <div className="fixedWrapper">
-      <Navigation/>
-      <div className="container mainWrapper">
-        <div className="row">
-          <Sidebar isMobile={false}/>
-          <div className="col-xs-12 col-md-9">
-            <div className="post">
-              <div className="postContainer">
-                <Switch>
-                  <Route exact path="/" component={PostList} />
-                  <Route path="/p/:page?" component={PostList} />
-                  <Route path="/post/:year/:month/:date/:slug?" component={PostComplete} />
-                  <Route path="/tags" component={Tags} />
-                  <Route path="/tag/:tag?" component={TagComplete} />
-                  <Route path="/search/:query?" component={Search} />
-                  <Route component={FourOhFourError} />
-                </Switch>
+function stateSelector({ sidebar }) {
+  return { sidebar}
+}
+
+@connect(stateSelector)
+class Routes extends React.Component {
+
+  render() {
+    const { sidebar } = this.props
+    const postClass = sidebar.isOpen ? 'col-xs-12 col-md-9' : 'col-xs-12 col-md-12'
+
+    return (
+      <Router>
+        <div className="fixedWrapper">
+          <Navigation/>
+          <div className="container mainWrapper">
+            <div className="row">
+              <Sidebar isMobile={false}/>
+              <div className={postClass}>
+                <div className="post">
+                  <div className="postContainer">
+                    <Switch>
+                      <Route exact path="/" component={PostList} />
+                      <Route path="/p/:page?" component={PostList} />
+                      <Route path="/post/:year/:month/:date/:slug?" component={PostComplete} />
+                      <Route path="/tags" component={Tags} />
+                      <Route path="/tag/:tag?" component={TagComplete} />
+                      <Route path="/search/:query?" component={Search} />
+                      <Route component={FourOhFourError} />
+                    </Switch>
+                  </div>
+                </div>
               </div>
             </div>
+            <Footer />
           </div>
         </div>
-        <Footer />
-      </div>
-    </div>
-  </Router>
-);
+      </Router>
+    )
+  }
+}
 
 export default Routes
